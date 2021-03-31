fea383d38334295ec20dc60df2de3b022ecb7301
nehers <59535194+nehers@users.noreply.github.com>
2021-03-31 06:47:53 +0200

    Disclaimer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'app1', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
@@ -10,7 +10,11 @@ const routes: Routes = [
   {
     path: 'app1',
     loadChildren: () => import('./app1/app1.module').then( m => m.App1PageModule)
+  },  {
+    path: 'impressum',
+    loadChildren: () => import('./impressum/impressum.module').then( m => m.ImpressumPageModule)
   },
+
   
 ];
 
